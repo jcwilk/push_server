d6d53b3a79c355f93aa26471733ea8f01b160b5d
jcwilk <jcwilk@gmail.com>
2011-02-20 16:40:02 -0800

    Testing concurrency

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World from Duostack!\n');
-}).listen(8124, "127.0.0.1");
+    var respond = function(){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Hello World from Duostack!\n');
+    };
+    setTimeout(respond,5000);
+}).listen(3000, "127.0.0.1");
 
-console.log('Server running at http://127.0.0.1:8124/');
+console.log('Server running at http://127.0.0.1:3000/');
