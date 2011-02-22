bd29d5e52a8754cd8ef36d27775c367e1afc724d
jcwilk <jcwilk@gmail.com>
2011-02-21 18:36:40 -0800

    Commented out the old code... shit sucks

--- public/push.js
+++ public/push.js
@@ -3,37 +3,37 @@ NodePush = function(){
     //USAGE:
     //var obamaTweets = "http://www.twitter.com/status/user_timeline/BARACKOBAMA.json?count=5&callback=JSONPCallback";
     //jsonp.fetch(obamaTweets, function(data) {console.log(data[0].text)});
-    var jsonp = {
-        callbackCounter: 0,
-
-        fetch: function(url, callback) {
-            var fn = 'JSONPCallback_' + this.callbackCounter++;
-            window[fn] = this.evalJSONP(callback);
-            url = url.replace('=JSONPCallback', '=' + fn);
-
-            var scriptTag = document.createElement('SCRIPT');
-            scriptTag.src = url;
-            document.getElementsByTagName('HEAD')[0].appendChild(scriptTag);
-        },
-
-        evalJSONP: function(callback) {
-            return function(data) {
-                var validJSON = false;
-                if (typeof data == "string") {
-                    try {validJSON = JSON.parse(data);} catch (e) {
-                        //console.warn('unable to parse: '+data)
-                    }
-                } else {
-                    validJSON = JSON.parse(JSON.stringify(data))
-                }
-                if (validJSON) {
-                    callback(validJSON);
-                } else {
-                    throw("JSONP call returned invalid or empty JSON");
-                }
-            }
-        }
-    };
+//    var jsonp = {
+//        callbackCounter: 0,
+//
+//        fetch: function(url, callback) {
+//            var fn = 'JSONPCallback_' + this.callbackCounter++;
+//            window[fn] = this.evalJSONP(callback);
+//            url = url.replace('=JSONPCallback', '=' + fn);
+//
+//            var scriptTag = document.createElement('SCRIPT');
+//            scriptTag.src = url;
+//            document.getElementsByTagName('HEAD')[0].appendChild(scriptTag);
+//        },
+//
+//        evalJSONP: function(callback) {
+//            return function(data) {
+//                var validJSON = false;
+//                if (typeof data == "string") {
+//                    try {validJSON = JSON.parse(data);} catch (e) {
+//                        //console.warn('unable to parse: '+data)
+//                    }
+//                } else {
+//                    validJSON = JSON.parse(JSON.stringify(data))
+//                }
+//                if (validJSON) {
+//                    callback(validJSON);
+//                } else {
+//                    throw("JSONP call returned invalid or empty JSON");
+//                }
+//            }
+//        }
+//    };
 
     function pollerFactory(channel,callback,sequence){
         var url = 'http://'+host+'/m/'+channel+'.json?';
